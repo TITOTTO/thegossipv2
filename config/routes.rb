7222619b1af921b1f8084cfddd6c5e4021d4ca96
TITOTTO <kuaker26@gmail.com>
2022-10-31 20:07:23 +0100

    mise en page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :gossip
-  resources :user
+  resources :user do
+    resources :gossip
+  end
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
